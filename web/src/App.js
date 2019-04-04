8455677ed736d44dbf02a86ce86da75cb819f978
ShellyXueHan <shelly.han@gov.bc.ca>
2019-04-04 14:47:07 -0700

    use react helmet

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Realm-o-Matic</title>
+          <meta name="description" content="Self-service App for KeyCloak Realm Provosioning" />
+        </Helmet>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
