f654f0932cd78682bb702a8046c9b8cbc72887ff
ShellyXueHan <shelly.han@gov.bc.ca>
2019-04-08 15:21:30 -0700

    enable passport

--- api/src/index.js
+++ api/src/index.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Authentication middleware
-// authmware(app);
+authmware(app);
 
 // Server API routes
 router(app);
