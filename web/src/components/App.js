390ff171fab0c6dbd38f3884434261c65a65734d
ShellyXueHan <shelly.han@gov.bc.ca>
2019-04-08 09:45:43 -0700

    use css module

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
-import typography from './utils/typography';
-import logo from './logo.svg';
-import './App.css';
+// eslint-disable-next-line
+import typography from '../utils/typography';
+import logo from '../assets/logo.svg';
+import styles from './App.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={styles.app}>
         <Helmet>
           <meta charSet="utf-8" />
           <title>Realm-o-Matic</title>
@@ -20,13 +21,13 @@ class App extends Component {
           />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         </Helmet>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
+            className={styles.appLink}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
