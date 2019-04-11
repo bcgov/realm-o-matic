1d38fe7d2c518012b88936b8c22c95190b40a3e6
ShellyXueHan <shelly.han@gov.bc.ca>
2019-04-11 12:29:48 -0700

    add semantic ui

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,41 +1,80 @@
 import React, { Component } from 'react';
-import Helmet from 'react-helmet';
-import logo from '../assets/logo.svg';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Button, List } from 'semantic-ui-react';
+import implicitAuthManager from '../utils/auth';
 import styles from './App.module.css';
+import { getIdps } from '../actionCreators';
+import { authenticateFailed, authenticateSuccess } from '../actions';
 
-class App extends Component {
+export class App extends Component {
+  componentDidMount = () => {
+    implicitAuthManager.registerHooks({
+      onAuthenticateSuccess: () => this.props.login(),
+      onAuthenticateFail: () => this.props.logout(),
+    });
+    // don't call function if on localhost
+    if (!window.location.host.match(/localhost/)) {
+      implicitAuthManager.handleOnPageLoad();
+    }
+    try {
+      const iamId = implicitAuthManager.idToken.data.sub;
+      const token = implicitAuthManager.idToken.bearer;
+    } catch (err) {
+      console.log('---implicitAuthManager----not logged in');
+    }
+  };
+
   render() {
+    const onClick = () => {
+      this.props.getIdps();
+    };
+
+    const onClickSSO = () => {
+      window.location = implicitAuthManager.getSSOLoginURI();
+    };
+
+    const idps = this.props.idps ? this.props.idps : [];
+    const buttons = !this.props.userId ? (
+      <Button onClick={onClickSSO}>sso login</Button>
+    ) : (
+      <Button onClick={onClick}>get idps</Button>
+    );
+
     return (
       <div className={styles.app}>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Realm-o-Matic</title>
-          <meta name="description" content="Self-service KeyCloak Realm Provosioner" />
-          <meta name="twitter:author" content="ShellyXueHan" />
-          <meta name="twitter:description" content="Self-service KeyCloak Realm Provosioner"
-          />
-          <meta name="og:author" content="ShellyXueHan" />
-          <meta name="og:description" content="Self-service KeyCloak Realm Provosioner"
-          />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        </Helmet>
-        <header className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={styles.appLink}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hello!!1
-          </a>
-        </header>
+        <p>Welcome!</p>
+        {buttons}
+        <p>{this.props.errorMessage}</p>
+        <List items={idps} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.authentication.isAuthenticated,
+    email: state.authentication.email,
+    userId: state.authentication.userId,
+    errorMessage: state.getIdps.errorMessage,
+    idps: state.getIdps.idps,
+    getIdpsStarted: state.getIdps.getIdpsStarted,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      login: () => dispatch(authenticateSuccess()),
+      logout: () => dispatch(authenticateFailed()),
+      getIdps,
+    },
+    dispatch
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
