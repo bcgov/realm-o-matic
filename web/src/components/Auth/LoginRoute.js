a21308a1adaf75fee4039be5595751ca8933f782
ShellyXueHan <shelly.han@gov.bc.ca>
2019-05-16 09:33:16 -0700

    redirect indication

--- web/src/components/Auth/LoginRoute.js
+++ web/src/components/Auth/LoginRoute.js
@@ -6,5 +6,5 @@ export const LoginRoute = ({ match }) => {
   const idp = match.params.idp;
   if (idp) implicitAuthManager.config.kcIDPHint = idp;
   window.location.assign(implicitAuthManager.getSSOLoginURI());
-  return null;
+  return <div>Your are being redirected to KeyCloak login page...</div>;
 };
