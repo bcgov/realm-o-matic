2b03c59ae6f99e6839015d40802144d8406fc370
ShellyXueHan <shelly.han@gov.bc.ca>
2019-05-21 10:01:37 -0700

    added more comments

--- api/src/router/routes/users.js
+++ api/src/router/routes/users.js
@@ -28,7 +28,7 @@ const router = new Router();
 
 /**
  * Get the user authorization status
- * TODO: more auth logic
+ * TODO: more auth logic, this is temporary:
  */
 router.get(
   '/auth/:userId',
