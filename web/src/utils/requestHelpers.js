2a1ae09adab7cea6b98b3b107ce9685c44c75a2a
ShellyXueHan <shelly.han@gov.bc.ca>
2019-05-22 16:24:24 -0700

    remove any json path packages to avoid double curly bracket

--- web/src/utils/requestHelpers.js
+++ web/src/utils/requestHelpers.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import jp from 'jsonpath';
+// import jp from 'jsonpath';
 import uuidv4 from 'uuid/v4';
 import { formDataToRequest, requestToFormData } from '../constants/form';
 
@@ -28,11 +28,9 @@ export const normalizeData = (inputData, schema) => {
     return _.mapValues(schema, p => {
       if (_.isObject(p)) return normalizeData(inputData, p);
       else {
-        // NOTE: query always returns an array, taking the first item from it:
-        const result = jp.query(inputData, `$..${p}`);
-        const data = result[0];
-        if (data === undefined) throw Error(`Failed to read data with key: ${p}`);
-        return data;
+        // TODO: add function for nested input data, read from key like 'key1.key2.key3'
+        const data = inputData[p];
+        return data ? data : null;
       }
     });
   }
