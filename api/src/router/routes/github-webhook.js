6098599bc3851f8139a1ec5acab683cf872e1f4b
ShellyXueHan <shelly.han@gov.bc.ca>
2019-07-10 12:04:17 -0700

    move mock to constants

--- api/src/router/routes/github-webhook.js
+++ api/src/router/routes/github-webhook.js
@@ -28,7 +28,7 @@ import { validateSchema } from '../../libs/utils';
 import { PR_CONTENT_SCHEMA, GITHUB_LABELS } from '../../constants';
 import { setMailer } from '../../libs/email-utils';
 import { EMAIL_TYPE_TO_PATH, PR_ACTIONS } from '../../constants/email';
-import { EMAIL_TEST_CONTENT } from '../../../__mocks__/email';
+import { EMAIL_TEST_CONTENT } from '../../constants/email-mock';
 
 const router = new Router();
 
