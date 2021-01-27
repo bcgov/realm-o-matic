aca8073dd945a6678c671c89875993181b3e0582
ShellyXueHan <shelly.han@gov.bc.ca>
2021-01-27 15:26:18 -0800

    test on caddy env var

--- web/src/constants/auth.js
+++ web/src/constants/auth.js
@@ -17,12 +17,11 @@
 //
 // Created by Shelly Xue Han
 //
-import config from '../config.json';
 
 export const SSO_CONFIG = {
-  baseURL: process.env.REACT_APP_SSO_BASE_URL || config.ssoBaseUrl,
-  realmName: process.env.REACT_APP_SSO_REALM_NAME || config.ssoRealmName,
-  clientId: process.env.REACT_APP_SSO_CLIENT_ID || config.ssoClientId,
+  baseURL: process.env.REACT_APP_SSO_BASE_URL || '{{env "REACT_APP_SSO_BASE_URL"}}',
+  realmName: process.env.REACT_APP_SSO_REALM_NAME || '{{env "REACT_APP_SSO_REALM_NAME"}}',
+  clientId: process.env.REACT_APP_SSO_CLIENT_ID || '{{env "REACT_APP_SSO_CLIENT_ID"}}',
 };
 
 export const SSO_IDP = {
